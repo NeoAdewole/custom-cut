db9cd8e541a3fa76afda67a072a7f28b3cb805a6
Matt Adewole <9janeo@gmail.com>
2023-12-28 19:18:49 -0600

    Creates masthead pattern

---
+++ patterns/masthead.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Title: Page Masthead
+ * Slug: customcut/masthead
+ * Categories: featured
+ * Description: A visual identifier ideal for top of a page contianing a featured image and page/post title
+ * 
+ **/
+?>
+<!-- wp:group {"className":"masthead","layout":{"type":"constrained"}} -->
+<div class="wp-block-group masthead">
+  <!-- wp:post-featured-image {"aspectRatio":"auto","align":"full","style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+  <!-- wp:group {"className":"info glow-effect","align":"full"} -->
+  <div class="wp-block-group info glow-effect">
+    <!-- wp:post-title {"align":"full"} /-->
+    <!-- wp:template-part {"slug":"post-meta","theme":"custom-cut","area":"uncategorized"} /-->
+    <svg class="glow-container"></svg>
+    <svg class="glow-line"></svg>
+  </div>
+</div>
+<!-- /wp:group -->
